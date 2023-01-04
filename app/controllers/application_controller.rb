24f68735f08697c0aed7592e2451440fb0ed7d53
edgar ludena <edgar.ludena90@gmail.com>
2023-01-03 20:40:46 -0500

    post and delete added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,30 @@
+require "sinatra"
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/' do
+    "Hello World"
   end
 
+  get '/movie' do 
+    movies = Movie.all 
+    movies.to_json
+  end
+
+  get '/movie/:id' do
+    movies = Movie.find(params[:id])
+    movies.to_json
+  end
+
+  post '/movie' do
+    movies =Movie.create(title:params[:title],genre:params[:genre],director:params[:director],year:params[:year])
+    movies.to_json
+
+  delete '/movie/:id' do
+    movie = Movie.find(params[:id])
+    movie.destroy
+    movie.to_json
+  end
 end
+
